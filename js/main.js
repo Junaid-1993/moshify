5ea1c426ea7dfebeeac4af2630208940f5bc4e0b
junaid-1993 <junaidali9001@gmail.com>
2024-02-25 12:20:52 +0500

    Create a homepage.

--- js/main.js
+++ js/main.js
@@ -5,3 +5,14 @@ collapsibles.forEach((item) => {
     this.classList.toggle("collapsible--expanded");
   });
 });
+
+// const width = 6.43 / Math.sqrt(Math.pow(20 / 9, 2) + 1);
+// const height = (20 / 9) * width.toFixed(2);
+
+// const dpi = 409 / 160;
+// const dp = 1080 / dpi.toFixed(2);
+
+// console.log(width);
+// console.log(height);
+// console.log(dpi);
+// console.log(dp);
